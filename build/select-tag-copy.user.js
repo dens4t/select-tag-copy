7083c730b382ef524704e903416d4035e22162f9
densat98 <denipontianak@gmail.com>
2022-07-28 22:03:53 +0700

    Release the build

---
+++ build/select-tag-copy.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Select Tag Copy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Can easily copy the value of select tag!
+// @author       Densat
+// @match        http://*/*
+// @match        https://*/*
+// @grant        none
+// @run-at       context-menu
+// ==/UserScript==
+let el = document.activeElement;
+if (el.tagName !== "span") el = el.closest("SPAN");
+if (el)prompt(el.innerText, el.innerText);
+else console.log('Not actually the select tag');
